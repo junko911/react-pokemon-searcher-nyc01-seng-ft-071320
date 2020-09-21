63d8c666b5fc757ff8f89d45d30961f485132964
junko911 <junkotahara911@gmail.com>
2020-09-21 11:23:16 -0400

    Add search feture

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -7,13 +7,22 @@ import { Container } from 'semantic-ui-react'
 class PokemonPage extends React.Component {
 
   state = {
-    pokemons: []
+    pokemons: [],
+    searchTerm: ""
   }
 
   componentDidMount() {
     fetch("http://localhost:3000/pokemon")
     .then(res => res.json())
     .then(pokemons => this.setState(() => ({pokemons: pokemons})))
+  }
+
+  getSearchTerm = e => {
+    this.setState({searchTerm: e.target.value})
+  }
+
+  filteredPokemons = () => {
+    return this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.searchTerm))
   }
 
   render() {
@@ -23,9 +32,9 @@ class PokemonPage extends React.Component {
         <br />
         <PokemonForm />
         <br />
-        <Search />
+        <Search searchTerm={this.state.searchTerm} getSearchTerm={this.getSearchTerm}/>
         <br />
-        <PokemonCollection pokemons={this.state.pokemons}/>
+        <PokemonCollection pokemons={this.filteredPokemons()}/>
       </Container>
     )
   }
