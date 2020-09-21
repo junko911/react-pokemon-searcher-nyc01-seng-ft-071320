aa3ccd2f0debaf3ecedcbc15f049eb4ebefae764
junko911 <junkotahara911@gmail.com>
2020-09-21 11:03:44 -0400

    Add togglable image

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state = {
+    clicked: false
+  }
+
+  togglePokemonImage = () => {
+    this.setState(() => ({clicked: !this.state.clicked}))
+  }
+
   render() {
+    const imageSrc = this.state.clicked ? this.props.pokemon.sprites.back : this.props.pokemon.sprites.front
+
     return (
       <Card>
-        <div>
+        <div onClick={this.togglePokemonImage}>
           <div className="image">
-            <img alt="oh no!" src={this.props.pokemon.sprites.front} />
+            <img alt="oh no!" src={imageSrc}/>
           </div>
           <div className="content">
             <div className="header">{this.props.pokemon.name}</div>
