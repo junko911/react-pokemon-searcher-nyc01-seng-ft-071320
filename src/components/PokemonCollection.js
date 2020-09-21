78e59d55ed83302501f629862b51da400431cf3e
junko911 <junkotahara911@gmail.com>
2020-09-21 10:57:00 -0400

    Render pokemon cards

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,11 +3,19 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+  genCards = () => {
+    return this.props.pokemons.map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon}/>)
+  }
+
   render() {
     return (
-      <Card.Group itemsPerRow={6}>
+      <div>
         <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
+        <Card.Group itemsPerRow={6}>
+          {this.genCards()}
+        </Card.Group>
+      </div>
     )
   }
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,6 +5,17 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+    .then(res => res.json())
+    .then(pokemons => this.setState(() => ({pokemons: pokemons})))
+  }
+
   render() {
     return (
       <Container>
@@ -14,7 +25,7 @@ class PokemonPage extends React.Component {
         <br />
         <Search />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.pokemons}/>
       </Container>
     )
   }
